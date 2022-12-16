807b56bfa46cc46a8d92be569f7475e7844347a3
Caleb9083 <calebosam12@gmail.com>
2022-12-16 19:00:15 +0000

    Create authController with signup

---
+++ controllers/authController.js
@@ -0,0 +1,10 @@
+const User = require("../models/userModel");
+
+exports.signup = async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(200).json({ status: "success", data: { newUser } });
+  } catch (err) {
+    res.status(400).json({ status: "fail", message: err.message });
+  }
+};

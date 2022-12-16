6a18108e99701ca49176907fcbed8e538be97db9
Caleb9083 <calebosam12@gmail.com>
2022-12-16 18:40:03 +0000

    Use bodyParser, cors and morgan middlewares Create the global error handler

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const logger = require("morgan");
 
 const app = express();
 
+//Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+app.use(logger("dev"));
+
+//Routes
 app.get("/", (req, res, next) => {
   res.json({
     status: "success",
     message: "Home endpoint is working",
   });
+});
+
+//Error handler
+app.use((err, req, res, next) => {
+  if (err) res.status(400).json({ status: "fail", message: "Bad request" });
 });
 
 module.exports = app;

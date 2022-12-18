a4a73462aa0a635f6d47af32c6454961c991cb59
Caleb9083 <calebosam12@gmail.com>
2022-12-18 11:22:18 +0000

    Create sign-in, in controller and integrate in user router

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const AppError = require("../utils/appError");
+const bcrypt = require("bcrypt");
 
 exports.signup = async (req, res, next) => {
   try {
@@ -20,6 +21,36 @@ exports.signup = async (req, res, next) => {
     newUser.password = undefined;
 
     res.status(200).json({ status: "success", token, data: { user: newUser } });
+  } catch (err) {
+    return next(new AppError(`${err.message}`, 400));
+  }
+};
+
+exports.signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(new AppError(`A user must provide email and password`, 400));
+    }
+
+    const user = await User.findOne({ email: email }).select("+password");
+
+    if (!user) {
+      return next(new AppError(`The user with this email does not exist`, 400));
+    }
+
+    const passwordCheck = await bcrypt.compare(password, user.password);
+
+    if (!user || !passwordCheck) {
+      return next(new AppError(`Email or password is incorrect`, 400));
+    }
+
+    const id = user.id;
+    const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_SECRET_EXPIRES_IN,
+    });
+
+    res.status(200).json({ status: "success", token, data: { user } });
   } catch (err) {
     return next(new AppError(`${err.message}`, 400));
   }

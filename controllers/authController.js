aa79f3fc5b672c509e2e54c0b396192a11d5bd42
Caleb9083 <calebosam12@gmail.com>
2022-12-18 10:30:52 +0000

    Use custom error class to handle user signup error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,26 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const AppError = require("../utils/appError");
 
 exports.signup = async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
-    res.status(200).json({ status: "success", data: { newUser } });
+    const newUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
+
+    const id = newUser._id;
+
+    const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_SECRET_EXPIRES_IN,
+    });
+
+    newUser.password = undefined;
+
+    res.status(200).json({ status: "success", token, data: { user: newUser } });
   } catch (err) {
-    res.status(400).json({ status: "fail", message: err.message });
+    return next(new AppError(`${err.message}`, 400));
   }
 };
